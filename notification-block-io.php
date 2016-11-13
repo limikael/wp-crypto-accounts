231eb496471b0f341b748c07940f33f6332e3ad1
Mikael Lindqvist <li.mikael@gmail.com>
2016-11-13 19:22:22 +0300

    bugfix

--- notification-block-io.php
+++ notification-block-io.php
@@ -1,4 +1,4 @@
 <?php
 
 $postdata = file_get_contents("php://input");
-file_put_contents(__DIR__."/tests/log/post.".utime().".html", $postdata);
+file_put_contents(__DIR__."/tests/log/post.".microtime(TRUE).".html", $postdata);
