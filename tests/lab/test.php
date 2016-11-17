ffc2bd4912c4f12c3f9ebf9c97cdf77f958a298a
Mikael Lindqvist <li.mikael@gmail.com>
2016-11-17 11:15:34 +0300

    test

--- tests/lab/test.php
+++ tests/lab/test.php
@@ -2,7 +2,7 @@
 
 error_reporting(E_ALL); ini_set('display_errors', 1);
 
-$f=popen("/bin/ls", "r");
-$s=fread($f,1000);
+$f=popen("/usr/bin/mkfifo 2>&1", "r");
+$s=fread($f,1000).fread($f,1000).fread($f,1000);
 
 echo $s;
